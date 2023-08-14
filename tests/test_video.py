3be23ce067cdc3f815850c39fd753baeba08ee0b
Serov Ivan <serovvans@yandex.ru>
2023-08-14 22:47:55 +0300

    add video class and tests

---
+++ tests/test_video.py
@@ -0,0 +1,6 @@
+def test_video(video1):
+    assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
+
+
+def test_pl_video(video2):
+    assert str(video2) == 'MoscowPython Meetup 78 - вступление'
